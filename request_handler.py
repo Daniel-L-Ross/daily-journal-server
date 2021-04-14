ab4730419fdd3b587a57261debd2d221d5be55c5
Dan Ross <daniellross@gmail.com>
2021-04-14 11:31:13 -0500

    added fales for requests and started on request handler for get all entries

---
+++ request_handler.py
@@ -0,0 +1,44 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+# add import statements from moods and entries here
+
+class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        return (resource, id)
+
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers(200)
+        response = {}
+
+        (resource, id) = self.parse_url(self.path)
+
+        if resouce == "entries":
+            if id is not None:
+                response = f"{get_single_entry(id)}"
+            
+            else:
+                response = f"{get_all_entries}"
